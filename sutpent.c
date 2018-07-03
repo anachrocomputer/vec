b3b04fae686e2ce4700887eebe56e7d90261ccf0
John Honniball <coredump@virginmedia.com>
2018-07-03 20:19:29 +0100

    Update to new coding standards.

--- sutpent.c
+++ sutpent.c
@@ -15,12 +15,12 @@ struct Vertex {
    double y;
 };
 
-void sutcliffepolygon (const double xc, const double yc, const double radius, const int nsides, const int levels, const double ratio);
-void sutpoly (const int nsides, const struct Vertex vertex[], const int level, const double ratio);
-void drawpoly (const int nsides, const struct Vertex vertex[]);
+void sutcliffepolygon(const double xc, const double yc, const double radius, const int nsides, const int levels, const double ratio);
+void sutpoly(const int nsides, const struct Vertex vertex[], const int level, const double ratio);
+void drawpoly(const int nsides, const struct Vertex vertex[]);
 
 
-int main (int argc, char * const argv[])
+int main(int argc, char * const argv[])
 {
    int opt;
    double w4, h4;
@@ -28,7 +28,7 @@ int main (int argc, char * const argv[])
    double maxx, maxy;
    double radius;
    
-   while ((opt = getopt (argc, argv, "no:p:s:t:v:")) != -1) {
+   while ((opt = getopt(argc, argv, "no:p:s:t:v:")) != -1) {
       switch (opt) {
       case 's':
       case 'n':
@@ -36,40 +36,40 @@ int main (int argc, char * const argv[])
       case 'p':
       case 't':
       case 'v':
-         plotopt (opt, optarg);
+         plotopt(opt, optarg);
          break;
       default: /* '?' */
-         fprintf (stderr, "Usage: %s [-p pen] [-s <size>] [-t title]\n",
+         fprintf(stderr, "Usage: %s [-p pen] [-s <size>] [-t title]\n",
                   argv[0]);
-         fprintf (stderr, "       <size> ::= A1 | A2 | A3 | A4 | A5\n");
-         exit (EXIT_FAILURE);
+         fprintf(stderr, "       <size> ::= A1 | A2 | A3 | A4 | A5\n");
+         exit(EXIT_FAILURE);
       }
    }
 
-   plotbegin (1);
+   plotbegin(1);
 
-   getplotsize (&maxx, &maxy);
+   getplotsize(&maxx, &maxy);
    
    w4 = maxx / 4.0;
    h4 = maxy / 4.0;
    
    radius = maxy / 4.0;
-   xc = sqrt (((2.0 * radius) * (2.0 * radius)) - (h4 * h4));
+   xc = sqrt(((2.0 * radius) * (2.0 * radius)) - (h4 * h4));
    xc = (2.0 * w4) - xc;
    
-   sutcliffepolygon (maxx - xc, h4 * 3.0, radius, 5, 6, 0.15);
-   sutcliffepolygon (xc,        h4 * 3.0, radius, 6, 7, 0.15);
-   sutcliffepolygon (maxx - xc, h4,       radius, 7, 7, 0.15);
-   sutcliffepolygon (xc,        h4,       radius, 8, 7, 0.15);
-   sutcliffepolygon (w4 * 2.0,  h4 * 2.0, radius, 9, 7, 0.15);
+   sutcliffepolygon(maxx - xc, h4 * 3.0, radius, 5, 6, 0.15);
+   sutcliffepolygon(xc,        h4 * 3.0, radius, 6, 7, 0.15);
+   sutcliffepolygon(maxx - xc, h4,       radius, 7, 7, 0.15);
+   sutcliffepolygon(xc,        h4,       radius, 8, 7, 0.15);
+   sutcliffepolygon(w4 * 2.0,  h4 * 2.0, radius, 9, 7, 0.15);
 
-   plotend ();
+   plotend();
    
    return (0);
 }
 
    
-void sutcliffepolygon (const double xc, const double yc, const double radius, const int nsides, const int levels, const double ratio)
+void sutcliffepolygon(const double xc, const double yc, const double radius, const int nsides, const int levels, const double ratio)
 {
    const double delta = (2.0 * M_PI) / (double)nsides;
    double theta;
@@ -78,16 +78,15 @@ void sutcliffepolygon (const double xc, const double yc, const double radius, co
    
    for (i = 0; i < nsides; i++) {
       theta = (double)i * delta;
-      vertex[i].x = xc + (cos (theta) * radius);
-      vertex[i].y = yc + (sin (theta) * radius);
+      vertex[i].x = xc + (cos(theta) * radius);
+      vertex[i].y = yc + (sin(theta) * radius);
    }
    
-// circle (xc, yc, radius);
-   sutpoly (nsides, vertex, levels, ratio);
+   sutpoly(nsides, vertex, levels, ratio);
 }
 
 
-void sutpoly (const int nsides, const struct Vertex vertex[], const int level, const double ratio)
+void sutpoly(const int nsides, const struct Vertex vertex[], const int level, const double ratio)
 {
    int i;
    int i1;
@@ -97,7 +96,7 @@ void sutpoly (const int nsides, const struct Vertex vertex[], const int level, c
    double x2, y2;
    struct Vertex newv[32];
    
-   drawpoly (nsides, vertex);
+   drawpoly(nsides, vertex);
    
    for (i = 0; i < nsides; i++) {
       i1 = (i + 1) % nsides;
@@ -126,28 +125,28 @@ void sutpoly (const int nsides, const struct Vertex vertex[], const int level, c
       x2 = x1 + dx;
       y2 = y1 + dy;
       
-      moveto (x1, y1);
-      lineto (x2, y2);
+      moveto(x1, y1);
+      lineto(x2, y2);
       
       newv[i].x = x2;
       newv[i].y = y2;
    }
    
    if (level > 0)
-      sutpoly (nsides, newv, level - 1, ratio);
+      sutpoly(nsides, newv, level - 1, ratio);
    else
-      drawpoly (nsides, newv);
+      drawpoly(nsides, newv);
 }
 
 
-void drawpoly (const int nsides, const struct Vertex vertex[])
+void drawpoly(const int nsides, const struct Vertex vertex[])
 {
    int i;
    
-   openlinesequence (vertex[0].x, vertex[0].y);
+   openlinesequence(vertex[0].x, vertex[0].y);
 
    for (i = 1; i < nsides; i++)
-      linesegmentto (vertex[i].x, vertex[i].y);
+      linesegmentto(vertex[i].x, vertex[i].y);
    
-   closelinesequence (1);
+   closelinesequence(1);
 }
