7fa9c24f58bc3f1757105bfea03038e2aabd00b0
John Honniball <coredump@virginmedia.com>
2019-04-17 22:50:09 +0100

    Finish the drawing by adding all the points of the stars. Not yet tested on a real plotter, though.

--- star_grid.c
+++ star_grid.c
@@ -12,12 +12,14 @@ void draw_star(const double x0, const double y0, const double gridsz, const doub
 
 int main(int argc, char * const argv[])
 {
-   /* Inspired by some Twitter image */
+   /* Inspired by Paul Rickards' tweet:
+      https://twitter.com/paulrickards/status/1096544602050514944
+    */
    int opt;
    int i, j;
    int ncells;
    double height;
-   double xc, yc;
+   double xc;
    double maxx, maxy;
    double left, right;
    double gridsz;
@@ -56,7 +58,6 @@ int main(int argc, char * const argv[])
    getplotsize(&maxx, &maxy);
    
    xc = maxx / 2.0;
-   yc = maxy / 2.0;
    height = maxy;
    gridsz = maxy / (double)ncells;
    left = xc - (height / 2.0);
@@ -88,7 +89,7 @@ int main(int argc, char * const argv[])
          const double xc = x0 + ((gridsz * (i + 1)) / (double)(ncells + 1));
          const double yc = y0 + ((gridsz * (j + 1)) / (double)(ncells + 1));
          
-         draw_star(x0, y0, gridsz, xc, yc, 7);
+         draw_star(x0, y0, gridsz, xc, yc, 9);
       }
    }
    
@@ -100,16 +101,42 @@ int main(int argc, char * const argv[])
 
 void draw_star(const double x0, const double y0, const double gridsz, const double xc, const double yc, const int n)
 {
-   moveto(xc, yc);
-   lineto(x0, y0);
+   int i;
+   const double d = gridsz / (double)(n - 1);
    
-   moveto(xc, yc);
-   lineto(x0 + gridsz, y0);
+   for (i = 0; i < (n / 2); i++) {
+      const double x1 = x0 + (i * d * 2.0);
+      const double x2 = x1 + d;
+
+      moveto(x1, y0);
+      lineto(xc, yc);
+      lineto(x2, y0);
+   }
    
-   moveto(xc, yc);
-   lineto(x0, y0 + gridsz);
+   for (i = 0; i < (n / 2); i++) {
+      const double y1 = y0 + (i * d * 2.0);
+      const double y2 = y1 + d;
+
+      moveto(x0 + gridsz, y1);
+      lineto(xc, yc);
+      lineto(x0 + gridsz, y2);
+   }
    
-   moveto(xc, yc);
-   lineto(x0 + gridsz, y0 + gridsz);
+   for (i = 0; i < (n / 2); i++) {
+      const double x1 = x0 + gridsz - (i * d * 2.0);
+      const double x2 = x1 - d;
+
+      moveto(x1, y0 + gridsz);
+      lineto(xc, yc);
+      lineto(x2, y0 + gridsz);
+   }
    
+   for (i = 0; i < (n / 2); i++) {
+      const double y1 = y0 + gridsz - (i * d * 2.0);
+      const double y2 = y1 - d;
+
+      moveto(x0, y1);
+      lineto(xc, yc);
+      lineto(x0, y2);
+   }
 }
