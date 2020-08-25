dbe073699e2b4c79fc9482bb27158dd006e8d540
John Honniball <coredump@virginmedia.com>
2020-08-25 22:09:43 +0100

    Remove 'drawline()' from 'allover13.c' to eliminate a call to deprecated 'hpglout()'. Not much point using it in the first place.

--- allover13.c
+++ allover13.c
@@ -14,7 +14,6 @@
 
 void draw_module(const double x, const double y, const double side, const int firstx, const int lastx, const int firsty, const int lasty);
 void square_triangle(const double x, const double y, const double side, const double ht);
-void drawline(const double wx1, const double wy1, const double wx2, const double wy2);
 
 double Scale;
 
@@ -90,15 +89,20 @@ void draw_module(const double x, const double y, const double side, const int fi
    double hside = side / 2.0;
    double offset = 3.0 * step;
    
-   drawline(x + offset, y + hside, x + side - offset, y + hside);
-   drawline(x + hside, y + offset, x + hside, y + side - offset);
+   // Draw central cross
+   moveto(x + offset,        y + hside);
+   lineto(x + side - offset, y + hside);
+   moveto(x + hside,         y + offset);
+   lineto(x + hside,         y + side - offset);
    
+   // Draw squares surrounded by triangles
    for (i = 0; i < 3; i++) {
       offset = (double)(i + 1) * step;
       trueside = side - (2.0 * step * (double)(i + 1));
       square_triangle(x + offset, y + offset, trueside, step);
    }
    
+   // Draw circles, allowing for special cases that draw arcs at the edges
    if (firsty) {
       moveto(x + radius, y);
 
@@ -156,47 +160,4 @@ void square_triangle(const double x, const double y, const double side, const do
    lineto(x + side, y);
    lineto(x + hside, y - ht);
    lineto(x, y);
-}
-
-
-void drawline(const double wx1, const double wy1, const double wx2, const double wy2)
-{
-   static int cx = -32767, cy = -32767;
-   static double wcx = -1.0, wcy = -1.0;
-   int x1, y1, x2, y2;
-   const double dx1 = wx1 - wcx;
-   const double dy1 = wy1 - wcy;
-   const double dx2 = wx2 - wcx;
-   const double dy2 = wy2 - wcy;
-   const double d1 = sqrt((dx1 * dx1) + (dy1 * dy1));
-   const double d2 = sqrt((dx2 * dx2) + (dy2 * dy2));
-   char hpgl[32];
-   
-   if (d1 < d2) {
-      x1 = getdevx(wx1);
-      y1 = getdevy(wy1);
-      x2 = getdevx(wx2);
-      y2 = getdevy(wy2);
-      wcx = wx2;
-      wcy = wy2;
-   }
-   else {
-      x1 = getdevx(wx2);
-      y1 = getdevy(wy2);
-      x2 = getdevx(wx1);
-      y2 = getdevy(wy1);
-      wcx = wx1;
-      wcy = wy1;
-   }
-   
-   if ((cx != x1) || (cy != y1)) {
-      snprintf(hpgl, sizeof (hpgl), "\nPU;PA%d,%d;", x1, y1);
-      hpglout(hpgl);
-   }
-
-   snprintf(hpgl, sizeof (hpgl), "PD;PA%d,%d;", x2, y2);
-   hpglout(hpgl);
-   
-   cx = x2;
-   cy = y2;
 }
