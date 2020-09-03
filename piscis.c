d2daa8d1810a4b175bf97675583b53fd1524def9
John Honniball <coredump@virginmedia.com>
2020-09-03 21:17:40 +0100

    Make 'piscis' into a four sub-plot drawing. Make it scale properly (no more '40.0' factors). Refactor code, correct comments.

--- piscis.c
+++ piscis.c
@@ -7,6 +7,10 @@
 #include <math.h>
 #include "hpgllib.h"
 
+void plot_ul(const double x0, const double y0, const double width, const double height, const double r1, const double r2);
+void plot_ur(const double x0, const double y0, const double width, const double height, const double r1, const double r2);
+void plot_ll(const double x0, const double y0, const double width, const double height, const double r1, const double r2);
+void plot_lr(const double x0, const double y0, const double width, const double height, const double r1, const double r2);
 
 int main(int argc, char * const argv[])
 {
@@ -44,21 +48,58 @@ int main(int argc, char * const argv[])
    xc = maxx / 2.0;
    yc = maxy / 2.0;
    
-   r1 = 100.0;
-   r2 = 120.0;
-
-   /* Two overlapping circles */
-   circle(xc - ((r1 / 2.0) * 40.0), yc, r1 * 40.0);
-   circle(xc + ((r1 / 2.0) * 40.0), yc, r1 * 40.0);
+   r1 = maxx / 5.0;
+   r2 = maxy / 5.0;
    
-   /* Round-ended oval around picis */
-   moveto(xc - ((r1 / 2.0) * 40.0), yc - (r2 * 40.0));
-   lineto(xc + ((r1 / 2.0) * 40.0), yc - (r2 * 40.0));
-   arc(xc + ((r1 / 2.0) * 40.0), yc, 180.0);
-   lineto(xc - ((r1 / 2.0) * 40.0), yc + (r2 * 40.0));
-   arc(xc - ((r1 / 2.0) * 40.0), yc, 180.0);
+   /* Split page into quarters */
+   moveto(0.0, yc);
+   lineto(maxx, yc);
+   moveto(xc, 0.0);
+   lineto(xc, maxy);
+   
+   /* Draw four circle plots */
+   plot_ll(0.0, 0.0, xc, yc, r1, r2);
+   plot_lr(xc,  0.0, xc, yc, r1, r2);
+   plot_ul(0.0, yc,  xc, yc, r1, r2);
+   plot_ur(xc,  yc,  xc, yc, r1, r2);
 
    plotend();
    
    return (0);
+}
+
+
+void plot_ul(const double x0, const double y0, const double width, const double height, const double r1, const double r2)
+{
+   /* https://en.wikipedia.org/wiki/Vesica_piscis */
+   const double xc = x0 + (width / 2.0);
+   const double yc = y0 + (height / 2.0);
+   const double r = r2 * 1.1;
+   const double d = r2 / 2.0;
+   
+   /* Two overlapping circles */
+   circle(xc - d, yc, r2);
+   circle(xc + d, yc, r2);
+   
+   /* Round-ended oval around vesica piscis */
+   moveto(xc - d, yc - r);
+   lineto(xc + d, yc - r);
+   arc(xc + d, yc, 180.0);
+   lineto(xc - d, yc + r);
+   arc(xc - d, yc, 180.0);
+}
+
+
+void plot_ur(const double x0, const double y0, const double width, const double height, const double r1, const double r2)
+{
+}
+
+
+void plot_ll(const double x0, const double y0, const double width, const double height, const double r1, const double r2)
+{
+}
+
+
+void plot_lr(const double x0, const double y0, const double width, const double height, const double r1, const double r2)
+{
 }
